4dc7ca87a2c0b2c84ce4b27396f89e320772796d
Lee juwon <aajun0120@gmail.com>
2024-04-10 14:50:26 +0900

    Week 7 - Day 2 Commit

---
+++ app.js
@@ -0,0 +1,23 @@
+// 20240409 이주원
+
+// express 모듈
+const express = require('express');
+const app = express();
+
+// dotenv 모듈
+const dotenv = require('dotenv');
+dotenv.config();
+
+app.listen(process.env.PORT);
+
+const userRouter = require('./routes/users');
+const orderRouter = require('./routes/orders');
+const likeRouter = require('./routes/likes');
+const cartRouter = require('./routes/carts');
+const bookRouter = require('./routes/books');
+
+app.use("/users", userRouter);
+app.use("/orders", orderRouter);
+app.use("/likes", likeRouter);
+app.use("/carts", cartRouter);
+app.use("/books", bookRouter);
